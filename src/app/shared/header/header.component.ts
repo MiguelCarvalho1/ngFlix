510e1df5a5a384917dcb1c6c80ddef32b9a068ea
migue <miguelangelosilvacarvalho96@gmail.com>
2024-04-23 15:50:33 +0100

    Application HomePage: Routes pages: home, movielist

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+
   templateUrl: './header.component.html',
+  imports: [
+    RouterLink
+  ],
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
